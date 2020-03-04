c20b2d1ba151c35fe184c0e739bb49c80ea91542
JoaoAdalberto <jadbnp18@gmail.com>
2020-03-03 21:35:18 -0300

    Att pyro
    Criando nameserver e o servidor, e atualizando  a caixa do chat para funcionar com o pyro

--- caixadochat.py
+++ caixadochat.py
@@ -24,27 +24,42 @@ class CaixaChat:
         self.texts = []
 
     # adiciona o texto para a janela de chat e verifica se tem 11, q e o tanto de mensagem q cabe na janela se tiver, deleta a primeira enviada
-    def adiciona_texto(self, jogador_atual, texto):
-        if len(self.chatarray) == 11:
-            self.chatarray.pop(0)
+    # def adiciona_texto(self, jogador_atual, texto):
+    #     if len(self.chatarray) == 11:
+    #         self.chatarray.pop(0)
+    #
+    #     self.chatarray.append((jogador_atual, texto))
+    #
+    # def pega_todos_textos(self):
+    #     return self.chatarray
+    #
+    # def reseta_interface(self):
+    #     self.screen.fill(branco)
 
-        self.chatarray.append((jogador_atual, texto))
 
-    def pega_todos_textos(self):
-        return self.chatarray
-
-    def reseta_interface(self):
-        self.screen.fill(branco)
-
+    def set_chatarray(self, chat):
+        self.chatarray = chat
     # manda as mensagems pra tela do chat nao to entendendo pq ta mudando a cor de todas as mensagems pra cor do ultimo que enviou a mensagem
-    def atualiza_tela_chatarray(self, jogador_atual):
-        for index, value in enumerate(self.chatarray):
-            (jogador_atual_da_mensagem, texto) = value
-            font = pygame.font.SysFont("Corbel", 20).render(texto, True, (0, 0, 0))
+    def atualiza_tela_chatarray(self):
+         index = 0
+         for mensagem in (self.chatarray):
+        #     (jogador_atual_da_mensagem, texto) = value
+            font = pygame.font.SysFont("Corbel", 20).render(mensagem, True, (0, 0, 0))
             render_font = font
             pygame.draw.rect(self.screen, brancola, (500, 90 + (30 * index), self.largura, 30))
             self.screen.blit(render_font, (510, 90 + (30 * index)))
             pygame.display.flip()
+            index += 1
+
+
+    # def atualiza_tela_chatarray(self, jogador_atual):
+    #     for index, value in enumerate(self.chatarray):
+    #         (jogador_atual_da_mensagem, texto) = value
+    #         font = pygame.font.SysFont("Corbel", 20).render(texto, True, (0, 0, 0))
+    #         render_font = font
+    #         pygame.draw.rect(self.screen, brancola, (500, 90 + (30 * index), self.largura, 30))
+    #         self.screen.blit(render_font, (510, 90 + (30 * index)))
+    #         pygame.display.flip()
             # if jogador_atual_da_mensagem == "verde":
             #     #texto = (f"Jogador verde: {texto}")
             #     font = pygame.font.SysFont("Corbel", 20).render(texto,True, green)
